39788363638089e1c150f1492adc26a0f1d2365c
FiFiTM <b.f.pabian@gmail.com>
2025-05-14 12:25:00 +0200

    Initial Commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Bartosz Pabian on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ helloWorldTests/helloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldTests.swift
+//  helloWorldTests
+//
+//  Created by Bartosz Pabian on 14/05/2025.
+//
+
+import Testing
+@testable import helloWorld
+
+struct helloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
